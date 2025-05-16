28a4676a451c98eb11f5f5a099002d9ed1f1826c
kalhara05 <kalhara139@gmail.com>
2025-05-17 00:14:00 +0530

    All Movie
    report

--- src/guiManager/MovieManage.java
+++ src/guiManager/MovieManage.java
@@ -559,7 +559,7 @@ public class MovieManage extends javax.swing.JPanel {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/zgencrms_db", "root", "Geeth@200104");
 
-            JasperPrint report = JasperFillManager.fillReport("src/reports/MovieNew.jasper", null, connection);
+            JasperPrint report = JasperFillManager.fillReport("src/reports/AMReport2.jasper", null, connection);
             JasperViewer.viewReport(report, false);
             
             connection.close();
